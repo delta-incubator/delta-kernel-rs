a00a82d23a8af5e4efa486ecfa62d4bb09dbca8e
Nick Lanham <nick@afternight.org>
2023-12-01 11:24:06 -0800

    add needed config for ifdefs down the road

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,6 +1,7 @@
 extern crate cbindgen;
 
 use cbindgen::{Config, Language};
+use std::collections::HashMap;
 use std::env;
 use std::path::PathBuf;
 
@@ -19,6 +20,15 @@ fn main() {
     let package_name = env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME should be set");
     let target_dir = get_target_dir(crate_dir.as_str());
 
+    // Any `cfgs` we want to turn into ifdefs need to go in here
+    let defines: HashMap<String, String> = HashMap::from([
+        (
+            "feature = default-client".into(),
+            "DEFINE_DEFAULT_CLIENT".into(),
+        ),
+        ("feature = tokio".into(), "DEFINE_TOKIO".into()),
+    ]);
+
     // generate cxx bindings
     let output_file_hpp = target_dir
         .join(format!("{}.hpp", package_name))
@@ -27,6 +37,7 @@ fn main() {
     let mut config_hpp = Config::default();
     config_hpp.language = Language::Cxx;
     config_hpp.namespace = Some(String::from("ffi"));
+    config_hpp.defines = defines.clone();
     cbindgen::generate_with_config(&crate_dir, config_hpp)
         .expect("generate_with_config should have worked for Cxx")
         .write_to_file(output_file_hpp);
@@ -38,6 +49,7 @@ fn main() {
         .to_string();
     let mut config_h = Config::default();
     config_h.language = Language::C;
+    config_h.defines = defines;
     cbindgen::generate_with_config(&crate_dir, config_h)
         .expect("generate_with_config should have worked for C")
         .write_to_file(output_file_h);
