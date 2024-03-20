c90cdff558d403198bc9fdf8e3d891f745fb81eb
Nick Lanham <nick@databricks.com>
2024-03-20 16:15:12 -0700

    fmt

--- acceptance/tests/dat_reader.rs
+++ acceptance/tests/dat_reader.rs
@@ -30,7 +30,9 @@ fn reader_test(path: &Path) -> datatest_stable::Result<()> {
             case.assert_metadata(engine_interface.clone())
                 .await
                 .unwrap();
-            acceptance::data::assert_scan_data(engine_interface.clone(), &case).await.unwrap();
+            acceptance::data::assert_scan_data(engine_interface.clone(), &case)
+                .await
+                .unwrap();
         });
     Ok(())
 }
