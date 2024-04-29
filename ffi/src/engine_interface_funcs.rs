842cb15648161b97c8fe6cafd24d8fbe8cc470d2
Nick Lanham <nick@databricks.com>
2024-04-29 16:36:18 -0700

    checkpoint

--- ffi/src/engine_interface_funcs.rs
+++ ffi/src/engine_interface_funcs.rs
@@ -1,8 +1,24 @@
 //! Generate functions to perform the "normal" engine interface operations
 
-use crate::{handle::ArcHandle, ExternEngineInterfaceHandle};
+use crate::{handle::ArcHandle, ExternEngineInterfaceHandle, KernelStringSlice};
 
-pub extern "C" fn parse_json(extern_table_client: *const ExternEngineInterfaceHandle) {
+#[repr(C)]
+pub struct FileMeta {
+    pub path: KernelStringSlice,
+    pub last_modified: i64,
+    pub size: usize,
+}
+
+impl Into<delta_kernel::FileMeta> for FileMeta {
+    fn into(self) -> delta_kernel::FileMeta {
+        todo!()
+    }
+}
+
+pub extern "C" fn read_parquet_files(
+    extern_table_client: *const ExternEngineInterfaceHandle,
+    file: &FileMeta
+) {
     let extern_table_client = unsafe { ArcHandle::clone_as_arc(extern_table_client) };
-    //extern_table_client.table_client().parse_json()
+    let interface = extern_table_client.table_client();
 }
