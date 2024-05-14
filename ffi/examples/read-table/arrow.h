e753f79ca2bcf7ee5e19944e137739f6fa1565bc
Nick Lanham <nick@databricks.com>
2024-05-14 14:41:54 -0700

    clean up GError reporting and unref some data

--- ffi/examples/read-table/arrow.h
+++ ffi/examples/read-table/arrow.h
@@ -19,14 +19,21 @@ ArrowContext* init_arrow_context() {
   return context;
 }
 
+// report and free an error if it's not NULL. Return true if error was not null, false otherwise
+static bool report_g_error(char* msg, GError* error) {
+  if (error != NULL) {
+    printf("%s: %s\n", msg, error->message);
+    g_error_free(error);
+    return true;
+  }
+  return false;
+}
+
 // Turn ffi formatted schema data into a GArrowSchema
 static GArrowSchema* get_schema(FFI_ArrowSchema* schema) {
   GError* error = NULL;
   GArrowSchema* garrow_schema = garrow_schema_import((gpointer)schema, &error);
-  if (error != NULL) {
-    fprintf(stderr, "Can't get schema: %s\n", error->message);
-    g_error_free(error);
-  }
+  report_g_error("Can't get schema", error);
   return garrow_schema;
 }
 
@@ -34,10 +41,7 @@ static GArrowSchema* get_schema(FFI_ArrowSchema* schema) {
 static GArrowRecordBatch* get_record_batch(FFI_ArrowArray* array, GArrowSchema* schema) {
   GError* error = NULL;
   GArrowRecordBatch* record_batch = garrow_record_batch_import((gpointer)array, schema, &error);
-  if (error != NULL) {
-    fprintf(stderr, "Can't get record batch: %s\n", error->message);
-    g_error_free(error);
-  }
+  report_g_error("Can't get record batch", error);
   return record_batch;
 }
 
@@ -50,6 +54,7 @@ static GArrowRecordBatch* add_partition_columns(GArrowRecordBatch* record_batch,
   gint64 rows = garrow_record_batch_get_n_rows(record_batch);
   gint64 cols = garrow_record_batch_get_n_columns(record_batch);
   GArrowRecordBatch* new_record_batch = record_batch;
+  GError* error = NULL;
   for (int i = 0; i < partition_cols->len; i++) {
     char* col = partition_cols->cols[i];
     guint pos = cols + i;
@@ -60,16 +65,30 @@ static GArrowRecordBatch* add_partition_columns(GArrowRecordBatch* record_batch,
         "  Adding partition column '%s' with value '%s' at column %u\n", col, partition_val, pos);
       GArrowStringArrayBuilder* builder = garrow_string_array_builder_new();
       for (gint64 i = 0; i < rows; i++) {
-        garrow_string_array_builder_append_string(builder, partition_val, NULL);
+        garrow_string_array_builder_append_string(builder, partition_val, &error);
+        if (report_g_error("Can't append to partition column builder", error)) {
+          break;
+        }
       }
-      GArrowArray* ret = garrow_array_builder_finish((GArrowArrayBuilder*)builder, NULL);
+      if (error != NULL) {
+        printf("Giving up on column %s\n", col);
+        g_object_unref(builder);
+        error = NULL;
+        continue;
+      }
+      GArrowArray* ret = garrow_array_builder_finish((GArrowArrayBuilder*)builder, &error);
+      if (report_g_error("Can't build string array for parition column", error)) {
+        printf("Giving up on column %s\n", col);
+        g_object_unref(builder);
+        error = NULL;
+        continue;
+      }
+      g_object_unref(builder);
       GArrowField* field = garrow_field_new(col, (GArrowDataType*)garrow_string_data_type_new());
-      GError* error = NULL;
       new_record_batch = garrow_record_batch_add_column(new_record_batch, pos, field, ret, &error);
       if (new_record_batch == NULL) {
         if (error != NULL) {
-          // Report error to user, and free error
-          fprintf(stderr, "Could not add column at %u: %s\n", pos, error->message);
+          printf("Could not add column at %u: %s\n", pos, error->message);
           g_error_free(error);
         }
       }
@@ -91,7 +110,9 @@ static void add_batch_to_context(ArrowContext* context,
   GArrowRecordBatch* record_batch = get_record_batch(&arrow_data->array, schema);
   if (context->cur_filter != NULL) {
     record_batch = garrow_record_batch_filter(record_batch, context->cur_filter, NULL, NULL);
-    context->cur_filter = NULL; // the filter is now part of the record batch (TODO: is this true?)
+    // unref the filter since we don't need it anymore
+    g_object_unref(context->cur_filter);
+    context->cur_filter = NULL;
   }
   record_batch = add_partition_columns(record_batch, partition_cols, partition_values);
   context->batches =
@@ -107,15 +128,31 @@ static void add_batch_to_context(ArrowContext* context,
 // which is 1 byte
 static GArrowBooleanArray* slice_to_arrow_bool_array(const KernelBoolSlice slice) {
   GArrowBooleanArrayBuilder* builder = garrow_boolean_array_builder_new();
+  GError* error = NULL;
   for (int i = 0; i < slice.len; i++) {
     gboolean val = slice.ptr[i] ? TRUE : FALSE;
-    garrow_boolean_array_builder_append_value(builder, val, NULL);
+    garrow_boolean_array_builder_append_value(builder, val, &error);
+    if (report_g_error("Can't append to boolean builder", error)) {
+      g_object_unref(builder);
+      break;
+    }
   }
-  GArrowArray* ret = garrow_array_builder_finish((GArrowArrayBuilder*)builder, NULL);
-  if (ret == NULL) {
-    printf("ERROR IN BUILDING\n");
+  if (error == NULL) {
+    GArrowArray* ret = garrow_array_builder_finish((GArrowArrayBuilder*)builder, &error);
+    g_object_unref(builder);
+    if (ret == NULL) {
+      printf("Error in building boolean array");
+      if (error != NULL) {
+        printf(": %s\n", error->message);
+        g_error_free(error);
+      } else {
+        printf(".\n");
+      }
+    }
+    return (GArrowBooleanArray*)ret;
+  } else {
+    return NULL;
   }
-  return (GArrowBooleanArray*)ret;
 }
 
 // This is the callback that will be called for each chunk of data read from the parquet file
@@ -154,6 +191,9 @@ void read_parquet_file(struct EngineContext* context,
   }
   if (selection_vector.len > 0) {
     GArrowBooleanArray* sel_array = slice_to_arrow_bool_array(selection_vector);
+    if (sel_array == NULL) {
+      printf("[WARN] Failed to get an arrow boolean array, selection vector will be ignored\n");
+    }
     context->arrow_context->cur_filter = sel_array;
   }
   FileReadResultIterator* read_iter = read_res.ok;
@@ -177,19 +217,15 @@ void print_arrow_context(ArrowContext* context) {
     GArrowSchema* schema = garrow_record_batch_get_schema(context->batches[0]);
     GArrowTable* table =
       garrow_table_new_record_batches(schema, context->batches, context->num_batches, &error);
-    if (error != NULL) {
-      // Report error to user, and free error
-      fprintf(stderr, "Can't create table from batches: %s\n", error->message);
-      g_error_free(error);
+    if (report_g_error("Can't create table from batches", error)) {
+      return;
     }
     gchar* out = garrow_table_to_string(table, &error);
-    if (error != NULL) {
-      fprintf(stderr, "Can't turn table into string: %s\n", error->message);
-      g_error_free(error);
-    } else {
+    if (!report_g_error("Can't turn table into string", error)) {
       printf("\nTable Data:\n-----------\n\n%s\n", out);
       g_free(out);
     }
+    g_object_unref(table);
   } else {
     printf("[No data]\n");
   }
