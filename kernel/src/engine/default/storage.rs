54364ddc63dc66b9561d6d64d6a424ef5566a181
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2024-07-09 22:32:43 +0200

    Use HdfsObjectStore if scheme is equal to hdfs/viewfs

--- kernel/src/engine/default/storage.rs
+++ kernel/src/engine/default/storage.rs
@@ -1,3 +1,4 @@
+use hdfs_native_object_store::HdfsObjectStore;
 use object_store::parse_url_opts as object_store_parse_url_opts;
 use object_store::path::Path;
 use object_store::{Error, ObjectStore};
@@ -9,5 +10,16 @@ where
     K: AsRef<str>,
     V: Into<String>,
 {
+    let scheme = url.scheme();
+    if scheme == "hdfs" || scheme == "viewfs" {
+        let options_map = options
+            .into_iter()
+            .map(|(k, v)| (k.as_ref().to_string(), v.into()))
+            .collect();
+        let store = HdfsObjectStore::with_config(url.as_str(), options_map)?;
+        let path = Path::parse(url.path())?;
+        return Ok((Box::new(store), path));
+    }
+
     object_store_parse_url_opts(url, options)
 }
