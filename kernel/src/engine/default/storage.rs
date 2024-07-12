3b718f555b0f3023f4b85d812ee6c90c6ae527a0
Jan Schutte <4732389+SchutteJan@users.noreply.github.com>
2024-07-12 14:28:02 +0200

    Move hdfs-native-object-store dependency to cloud feature

--- kernel/src/engine/default/storage.rs
+++ kernel/src/engine/default/storage.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "cloud")]
 use hdfs_native_object_store::HdfsObjectStore;
 use object_store::parse_url_opts as object_store_parse_url_opts;
 use object_store::path::Path;
@@ -10,15 +11,18 @@ where
     K: AsRef<str>,
     V: Into<String>,
 {
-    let scheme = url.scheme();
-    if scheme == "hdfs" || scheme == "viewfs" {
-        let options_map = options
-            .into_iter()
-            .map(|(k, v)| (k.as_ref().to_string(), v.into()))
-            .collect();
-        let store = HdfsObjectStore::with_config(url.as_str(), options_map)?;
-        let path = Path::parse(url.path())?;
-        return Ok((Box::new(store), path));
+    #[cfg(feature = "cloud")]
+    {
+        let scheme = url.scheme();
+        if scheme == "hdfs" || scheme == "viewfs" {
+            let options_map = options
+                .into_iter()
+                .map(|(k, v)| (k.as_ref().to_string(), v.into()))
+                .collect();
+            let store = HdfsObjectStore::with_config(url.as_str(), options_map)?;
+            let path = Path::parse(url.path())?;
+            return Ok((Box::new(store), path));
+        }
     }
 
     object_store_parse_url_opts(url, options)
