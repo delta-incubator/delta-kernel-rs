9af9baedbe1b3743acddf1f7206a1ca8f08b9dd3
Nick Lanham <nick@databricks.com>
2024-07-19 10:09:18 -0700

    more info from script

--- ffi/tests/read-table-testing/run_test.sh
+++ ffi/tests/read-table-testing/run_test.sh
@@ -3,9 +3,13 @@
 set -euxo pipefail
 
 OUT_FILE=$(mktemp)
-./read_table "$1" > $OUT_FILE
-diff -s $OUT_FILE "$2"
+pwd
+ls -l
+./read_table "$1" > "$OUT_FILE"
+cat "$OUT_FILE"
+diff -s "$OUT_FILE" "$2"
 DIFF_EXIT_CODE=$?
+echo "Diff exited with $DIFF_EXIT_CODE"
 rm "$OUT_FILE"
 exit "$DIFF_EXIT_CODE"
 
