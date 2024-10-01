35c08347a6fd4d71854865872b3ca12bf535dc9c
Nick Lanham <nick.lanham@pm.me>
2024-10-01 15:37:29 -0700

    add simple "integration" test

---
+++ integration-tests/test-all-arrow-versions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+is_version_le() {
+    [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+}
+
+is_version_lt() {
+  if [ "$1" = "$2" ]
+  then
+    return 1
+  else
+    is_version_le "$1" "$2"
+  fi
+}
+
+test_arrow_version() {
+  ARROW_VERSION="$1"
+  sed -i "s/\(arrow[^\"]*=[^\"]*\).*/\1\"=$ARROW_VERSION\"/" Cargo.toml
+  sed -i "s/\(parquet[^\"]*\).*/\1\"=$ARROW_VERSION\"/" Cargo.toml
+  cargo clean
+  rm Cargo.lock
+  cargo update
+  cargo run
+}
+
+MIN_ARROW_VER="52.0.0"
+
+for ARROW_VERSION in $(curl -s https://crates.io/api/v1/crates/arrow | jq -r '.versions[].num')
+do
+  if ! is_version_lt "$ARROW_VERSION" "$MIN_ARROW_VER"
+  then
+    test_arrow_version "$ARROW_VERSION"
+  fi
+done
