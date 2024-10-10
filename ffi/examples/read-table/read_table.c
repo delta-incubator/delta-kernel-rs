295e8e81de4a4cc545527d29dacb97534f1cc9e4
Oussama Saoudi <oussama.saoudi@databricks.com>
2024-10-10 16:49:20 -0700

    Move testing code into separate executable

--- ffi/examples/read-table/read_table.c
+++ ffi/examples/read-table/read_table.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include "arrow.h"
-#include "expression.h"
 #include "read_table.h"
 #include "schema.h"
 
@@ -106,7 +105,11 @@ void scan_row_callback(
 {
   (void)size; // not using this at the moment
   struct EngineContext* context = engine_context;
-  print_diag("Called back to read file: %.*s. (size: %" PRIu64 ", num records: ", (int)path.len, path.ptr, size);
+  print_diag(
+    "Called back to read file: %.*s. (size: %" PRIu64 ", num records: ",
+    (int)path.len,
+    path.ptr,
+    size);
   if (stats) {
     print_diag("%" PRId64 ")\n", stats->num_records);
   } else {
