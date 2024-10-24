4faff29afb5141743fb6c13f25a476dec61f3ba8
Oussama Saoudi <oussama.saoudi@databricks.com>
2024-10-24 14:50:32 -0700

    Add dev visibility

--- kernel/src/expressions/scalars.rs
+++ kernel/src/expressions/scalars.rs
@@ -20,6 +20,7 @@ pub struct ArrayData {
 }
 
 impl ArrayData {
+    #[cfg_attr(feature = "developer-visibility", visibility::make(pub))]
     pub fn new(tpe: ArrayType, elements: impl IntoIterator<Item = impl Into<Scalar>>) -> Self {
         let elements = elements.into_iter().map(Into::into).collect();
         Self { tpe, elements }
