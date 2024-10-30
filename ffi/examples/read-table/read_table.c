40dc2b5badf87376b2e2371a9a33223992ab460e
Nick Lanham <nicklan@users.noreply.github.com>
2024-10-30 15:44:27 -0700

    fix leaks in read_table (#449)
    <!--
    Thanks for sending a pull request!  Here are some tips for you:
    1. If this is your first time, please read our contributor guidelines:
    https://github.com/delta-incubator/delta-kernel-rs/blob/main/CONTRIBUTING.md
    2. Run `cargo t --all-features --all-targets` to get started testing,
    and run `cargo fmt`.
      3. Ensure you have added or run the appropriate tests for your PR.
    4. If the PR is unfinished, add '[WIP]' in your PR title, e.g., '[WIP]
    Your PR title ...'.
      5. Be sure to keep the PR description updated to reflect all changes.
    -->
    
    ## What changes are proposed in this pull request?
    <!--
    Please clarify what changes you are proposing and why the changes are
    needed.
    The purpose of this section is to outline the changes, why they are
    needed, and how this PR fixes the issue.
    If the reason for the change is already explained clearly in an issue,
    then it does not need to be restated here.
    1. If you propose a new API or feature, clarify the use case for a new
    API or feature.
      2. If you fix a bug, you can clarify why it is a bug.
    -->
    
    Fix leaks in `read_table`. It now runs cleanly with
    `-fsanitize=address`.
    
    Some of these are probably footguns in kernel that we should fix. I've
    also added comments for now in kernel for some places where engine is
    responsible to free things. We should look at these and see if we
    can/should move to a callback model so the freeing is not necessary.
    
    The good news is, this didn't find any leaks in kernel itself, just bad
    c code :)
    
    Fixes #448 
    
    <!--
    Uncomment this section if there are any changes affecting public APIs:
    ### This PR affects the following public APIs
    
    If there are breaking changes, please ensure the `breaking-changes`
    label gets added by CI, and describe why the changes are needed.
    
    Note that _new_ public APIs are not considered breaking.
    -->
    
    
    ## How was this change tested?
    <!--
    Please make sure to add test cases that check the changes thoroughly
    including negative and positive cases if possible.
    If it was tested in a way different from regular unit tests, please
    clarify how you tested, ideally via a reproducible test documented in
    the PR description.
    -->
    - ffi test ci job doesn't report leaks
    - Locally running and observing sanitizer output. Also checked, no more
    direct leaks reported by `valgrind`

--- ffi/examples/read-table/read_table.c
+++ ffi/examples/read-table/read_table.c
@@ -6,10 +6,6 @@
 #include "read_table.h"
 #include "schema.h"
 
-// TODO - We cannot enable leak detection until existing leaks are fixed.
-// See https://github.com/delta-incubator/delta-kernel-rs/issues/448
-const char* __asan_default_options() { return "detect_leaks=0"; }
-
 // some diagnostic functions
 void print_diag(char* fmt, ...)
 {
@@ -151,6 +147,7 @@ void do_visit_scan_data(
   print_diag("Asking kernel to call us back for each scan row (file to read)\n");
   visit_scan_data(engine_data, selection_vec, engine_context, scan_row_callback);
   free_bool_slice(selection_vec);
+  free_engine_data(engine_data);
 }
 
 // Called for each element of the partition StringSliceIterator. We just turn the slice into a
@@ -195,6 +192,14 @@ PartitionList* get_partition_list(SharedGlobalScanState* state)
   }
   free_string_slice_data(part_iter);
   return list;
+}
+
+void free_partition_list(PartitionList* list) {
+  for (int i = 0; i < list->len; i++) {
+    free(list->cols[i]);
+  }
+  free(list->cols);
+  free(list);
 }
 
 int main(int argc, char* argv[])
@@ -310,11 +315,13 @@ int main(int argc, char* argv[])
 #endif
 
   free_kernel_scan_data(data_iter);
+  free_scan(scan);
   free_global_read_schema(read_schema);
   free_global_scan_state(global_state);
   free_snapshot(snapshot);
   free_engine(engine);
   free(context.table_root);
+  free_partition_list(context.partition_cols);
 
   return 0;
 }
