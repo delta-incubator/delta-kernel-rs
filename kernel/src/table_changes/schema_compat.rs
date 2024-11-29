ba3d0ada4c13655990f1637b9cee89f7378209c6
Oussama Saoudi <oussama.saoudi@databricks.com>
2024-11-28 16:24:41 -0800

    Add schema compatibility tests

--- kernel/src/table_changes/schema_compat.rs
+++ kernel/src/table_changes/schema_compat.rs
@@ -2,17 +2,14 @@ use std::collections::{HashMap, HashSet};
 
 use crate::schema::{DataType, Schema, StructField, StructType};
 
-fn is_schema_compatible(existing: &Schema, new: &Schema) -> bool {
-    for field in later.fields() {}
-    todo!()
+fn is_nullability_compatible(existing_nullable: bool, read_nullable: bool) -> bool {
+    // The case to avoid is when the read_schema is non-nullable and the existing one is nullable.
+    // So we avoid the case where !read_nullable && existing_nullable
+    // Hence we check that !(!read_nullable && existing_nullable)
+    // == read_nullable || !existing_nullable
+    read_nullable || !existing_nullable
 }
-fn is_nullability_compatible(existing_nullable: bool, new_nullable: bool) -> bool {
-    // The case to avoid is when the new is non-nullable, but the existing one is.
-    // Hence !(!new_nullable && existing_nullable)
-    //  == new_nullable || !existing_nullable
-    new_nullable || !existing_nullable
-}
-fn is_struct_read_compatible(existing: &StructType, newtype: &StructType) -> bool {
+fn is_struct_read_compatible(existing: &StructType, read_type: &StructType) -> bool {
     // Delta tables do not allow fields that differ in name only by case
     let existing_fields: HashMap<&String, &StructField> = existing
         .fields()
@@ -23,39 +20,34 @@ fn is_struct_read_compatible(existing: &StructType, newtype: &StructType) -> boo
         .fields()
         .map(|field| field.name().clone())
         .collect();
-    let new_names: HashSet<String> = newtype.fields().map(|field| field.name().clone()).collect();
-    if new_names.is_subset(&existing_names) {
+    let read_names: HashSet<String> = read_type
+        .fields()
+        .map(|field| field.name().clone())
+        .collect();
+    if !existing_names.is_subset(&read_names) {
         return false;
     }
-    newtype.fields().all(|new_field| {
-        existing_fields
-            .get(new_field.name())
-            .into_iter()
-            .all(|existing_field| {
-                existing_field.name() == new_field.name()
-                    && is_nullability_compatible(existing_field.nullable, new_field.nullable)
-                    && is_datatype_read_compatible(
-                        existing_field.data_type(),
-                        new_field.data_type(),
-                    )
-            })
-    });
-    //
-    //    new_fields{ newField =>
-    //      // new fields are fine, they just won't be returned
-    //      existingFields.get(newField.name).forall { existingField =>
-    //        // we know the name matches modulo case - now verify exact match
-    //        (existingField.name == newField.name
-    //          // if existing value is non-nullable, so should be the new value
-    //          && isNullabilityCompatible(existingField.nullable, newField.nullable)
-    //          // and the type of the field must be compatible, too
-    //          && isDatatypeReadCompatible(existingField.dataType, newField.dataType))
-    //      }
-    //    }
-    false
+    read_type
+        .fields()
+        .all(|read_field| match existing_fields.get(read_field.name()) {
+            Some(existing_field) => {
+                let name_equal = existing_field.name() == read_field.name();
+
+                let nullability_equal =
+                    is_nullability_compatible(existing_field.nullable, read_field.nullable);
+                let data_type_equal =
+                    is_datatype_read_compatible(existing_field.data_type(), read_field.data_type());
+                println!(
+                    "name_equal {} nullability: {}, datatype: {}",
+                    name_equal, nullability_equal, data_type_equal
+                );
+                name_equal && nullability_equal && data_type_equal
+            }
+            None => read_field.is_nullable(),
+        })
 }
-fn is_datatype_read_compatible(existing: &DataType, newtype: &DataType) -> bool {
-    match (existing, newtype) {
+fn is_datatype_read_compatible(existing: &DataType, read_type: &DataType) -> bool {
+    match (existing, read_type) {
         // TODO: Add support for type widening
         (DataType::Array(a), DataType::Array(b)) => {
             is_datatype_read_compatible(a.element_type(), b.element_type())
@@ -71,74 +63,240 @@ fn is_datatype_read_compatible(existing: &DataType, newtype: &DataType) -> bool 
     }
 }
 
-//def isReadCompatible(
-//    existingSchema: StructType,
-//    readSchema: StructType,
-//    forbidTightenNullability: Boolean = false,
-//    allowMissingColumns: Boolean = false,
-//    allowTypeWidening: Boolean = false,
-//    newPartitionColumns: Seq[String] = Seq.empty,
-//    oldPartitionColumns: Seq[String] = Seq.empty): Boolean = {
-//
-//  def isNullabilityCompatible(existingNullable: Boolean, readNullable: Boolean): Boolean = {
-//    if (forbidTightenNullability) {
-//      readNullable || !existingNullable
-//    } else {
-//      existingNullable || !readNullable
-//    }
-//  }
-//
-//  def isDatatypeReadCompatible(existing: DataType, newtype: DataType): Boolean = {
-//    (existing, newtype) match {
-//      case (e: StructType, n: StructType) =>
-//        isReadCompatible(e, n, forbidTightenNullability, allowTypeWidening = allowTypeWidening)
-//      case (e: ArrayType, n: ArrayType) =>
-//        // if existing elements are non-nullable, so should be the new element
-//        isNullabilityCompatible(e.containsNull, n.containsNull) &&
-//          isDatatypeReadCompatible(e.elementType, n.elementType)
-//      case (e: MapType, n: MapType) =>
-//        // if existing value is non-nullable, so should be the new value
-//        isNullabilityCompatible(e.valueContainsNull, n.valueContainsNull) &&
-//          isDatatypeReadCompatible(e.keyType, n.keyType) &&
-//          isDatatypeReadCompatible(e.valueType, n.valueType)
-//      case (e: AtomicType, n: AtomicType) if allowTypeWidening =>
-//        TypeWidening.isTypeChangeSupportedForSchemaEvolution(e, n)
-//      case (a, b) => a == b
-//    }
-//  }
-//
-//  def isStructReadCompatible(existing: StructType, newtype: StructType): Boolean = {
-//    val existingFields = toFieldMap(existing)
-//    // scalastyle:off caselocale
-//    val existingFieldNames = existing.fieldNames.map(_.toLowerCase).toSet
-//    assert(existingFieldNames.size == existing.length,
-//      "Delta tables don't allow field names that only differ by case")
-//    val newFields = newtype.fieldNames.map(_.toLowerCase).toSet
-//    assert(newFields.size == newtype.length,
-//      "Delta tables don't allow field names that only differ by case")
-//    // scalastyle:on caselocale
-//
-//    if (!allowMissingColumns &&
-//      !(existingFieldNames.subsetOf(newFields) &&
-//        isPartitionCompatible(newPartitionColumns, oldPartitionColumns))) {
-//      // Dropped a column that was present in the DataFrame schema
-//      return false
-//    }
-//    newtype.forall { newField =>
-//      // new fields are fine, they just won't be returned
-//      existingFields.get(newField.name).forall { existingField =>
-//        // we know the name matches modulo case - now verify exact match
-//        (existingField.name == newField.name
-//          // if existing value is non-nullable, so should be the new value
-//          && isNullabilityCompatible(existingField.nullable, newField.nullable)
-//          // and the type of the field must be compatible, too
-//          && isDatatypeReadCompatible(existingField.dataType, newField.dataType))
-//      }
-//    }
-//  }
-//
-//  isStructReadCompatible(existingSchema, readSchema)
-//}
-//
+fn is_partition_compatible(
+    existing_partition_cols: &[String],
+    read_partition_cols: &[String],
+) -> bool {
+    existing_partition_cols == read_partition_cols
+}
+
+fn is_schema_compatible(existing_schema: &Schema, read_schema: &Schema) -> bool {
+    is_struct_read_compatible(existing_schema, read_schema)
+}
 #[cfg(test)]
-mod tests {}
+mod tests {
+
+    use crate::schema::{ArrayType, DataType, MapType, StructField, StructType};
+
+    use super::is_schema_compatible;
+
+    #[test]
+    fn equal_schema() {
+        let map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let map_value = StructType::new([StructField::new("age", DataType::INTEGER, true)]);
+        let map_type = MapType::new(map_key, map_value, true);
+
+        let array_type = ArrayType::new(DataType::TIMESTAMP, false);
+
+        let nested_struct = StructType::new([
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("map", map_type, false),
+            StructField::new("array", array_type, false),
+            StructField::new("nested_struct", nested_struct, false),
+        ]);
+
+        assert!(is_schema_compatible(&schema, &schema));
+    }
+
+    #[test]
+    fn different_schema_fails() {
+        let existing_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([
+            StructField::new("company", DataType::STRING, false),
+            StructField::new("employee_name", DataType::STRING, false),
+            StructField::new("salary", DataType::LONG, false),
+            StructField::new("position_name", DataType::STRING, true),
+        ]);
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+
+    #[test]
+    fn map_nullability_and_ok_schema_evolution() {
+        let existing_map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let existing_map_value =
+            StructType::new([StructField::new("age", DataType::INTEGER, true)]);
+        let existing_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(existing_map_key, existing_map_value, false),
+            false,
+        )]);
+
+        let read_map_key = StructType::new([
+            StructField::new("id", DataType::LONG, true),
+            StructField::new("name", DataType::STRING, true),
+            StructField::new("location", DataType::STRING, true),
+        ]);
+        let read_map_value = StructType::new([
+            StructField::new("age", DataType::INTEGER, true),
+            StructField::new("years_of_experience", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(read_map_key, read_map_value, true),
+            false,
+        )]);
+
+        assert!(is_schema_compatible(&existing_schema, &read_schema));
+    }
+    #[test]
+    fn map_value_becomes_non_nullable_fails() {
+        let map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let map_value = StructType::new([StructField::new("age", DataType::INTEGER, true)]);
+        let existing_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(map_key, map_value, false),
+            false,
+        )]);
+
+        let map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let map_value = StructType::new([StructField::new("age", DataType::INTEGER, false)]);
+        let read_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(map_key, map_value, false),
+            false,
+        )]);
+
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+    #[test]
+    fn map_schema_new_non_nullable_value_fails() {
+        let existing_map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let existing_map_value =
+            StructType::new([StructField::new("age", DataType::INTEGER, true)]);
+        let existing_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(existing_map_key, existing_map_value, false),
+            false,
+        )]);
+
+        let read_map_key = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+        ]);
+        let read_map_value = StructType::new([
+            StructField::new("age", DataType::INTEGER, true),
+            StructField::new("years_of_experience", DataType::INTEGER, false),
+        ]);
+        let read_schema = StructType::new([StructField::new(
+            "map",
+            MapType::new(read_map_key, read_map_value, false),
+            false,
+        )]);
+
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+
+    #[test]
+    fn different_field_name_fails() {
+        let existing_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([
+            StructField::new("new_id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+
+    #[test]
+    fn different_type_fails() {
+        let existing_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([
+            StructField::new("id", DataType::INTEGER, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+    #[test]
+    fn set_nullable_to_true() {
+        let existing_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, true),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        assert!(is_schema_compatible(&existing_schema, &read_schema));
+    }
+    #[test]
+    fn set_nullable_to_false_fails() {
+        let existing_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+        let read_schema = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, false),
+        ]);
+        assert!(!is_schema_compatible(&existing_schema, &read_schema));
+    }
+    #[test]
+    fn new_nullable_column() {
+        let existing = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+
+        let read = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+            StructField::new("location", DataType::STRING, true),
+        ]);
+        assert!(is_schema_compatible(&existing, &read));
+    }
+
+    #[test]
+    fn new_non_nullable_column_fails() {
+        let existing = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+        ]);
+
+        let read = StructType::new([
+            StructField::new("id", DataType::LONG, false),
+            StructField::new("name", DataType::STRING, false),
+            StructField::new("age", DataType::INTEGER, true),
+            StructField::new("location", DataType::STRING, false),
+        ]);
+        assert!(!is_schema_compatible(&existing, &read));
+    }
+}
